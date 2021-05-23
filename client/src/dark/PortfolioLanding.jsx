d1ccbdd0b9843695bd874b325b2c0bc854ce1dae
Todd Malone <toddomalone@gmail.com>
2021-05-22 22:06:43 -0500

    removed blog

--- client/src/dark/PortfolioLanding.jsx
+++ client/src/dark/PortfolioLanding.jsx
@@ -58,7 +58,7 @@ const PortfolioLanding = () => {
             </div>
             
             {/* End About Area */}
-            
+
             {/* Start Portfolio Area */}
             <div id="portfolio" className="fix">
                 <div className="portfolio-area ptb--120 bg_color--1">
@@ -87,43 +87,6 @@ const PortfolioLanding = () => {
                 </div>
             </div>
             {/* End Portfolio Area */}
-
-            {/* Start Blog Area */}
-            <div id="blog" className="fix">
-                <div className="rn-blog-area ptb--120 bg_color--5 mb-dec--30">
-                    <div className="container">
-                        <div className="row align-items-end">
-                            <div className="col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div className="section-title text-center">
-                                    <h2>Latest News</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available, <br />but the majority have suffered alteration.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row mt--60 mt_sm--40">
-                            {PostList.map((value , i ) => (
-                                <div className="col-lg-4 col-md-6 col-12" key={i}>
-                                    <div className="blog blog-style--1">
-                                        <div className="thumbnail">
-                                            <a href="/blog-details">
-                                                <img className="w-100" src={`/assets/images/blog/blog-${value.images}.jpg`} alt="Blog Images"/>
-                                            </a>
-                                        </div>
-                                        <div className="content">
-                                            <p className="blogtype">{value.category}</p>
-                                            <h4 className="title"><a href="/blog-details">{value.title}</a></h4>
-                                            <div className="blog-btn">
-                                                <a className="rn-btn text-white" href="/blog-details">Read More</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>    
-                    </div>    
-                </div>
-            </div>
-            {/* End Blog Area */}
 
             {/* Start COntact Area */}
             <div id="contact" className="fix">
