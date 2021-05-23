95e17cf74834a37a168d57322958b37760ab9934
Todd Malone <toddomalone@gmail.com>
2021-05-22 22:23:25 -0500

    created object stores for skill information

---
+++ client/src/profile-data/aboutMe.js
@@ -0,0 +1,50 @@
+const AboutMe = "I'm a software developer with experience in a wide variety of languages and tools. I am most experiecned with back-end languages and database interaction, and am now engaged in learning front-end development. I excel at projects with close collaboration and in mentorship roles"
+const Skills=  [
+    {
+        title: "Main skills",
+        items: [
+            {
+                header:"User experience design",
+                sub:"UI/UX",
+                description:"Delight the user and make it work."
+            },
+            {
+                header:"Web and user interface design",
+                sub:"Development",
+                description:"Websites, web experiences, ..."
+            },
+            {
+                header:"Interaction design ",
+                sub:"Animation",
+                description:"I like to move it move it."
+            }
+        ]
+    },
+    {
+        title: "Experience",
+        items: [
+            {
+                header:"Software Developer",
+                sub:"Open Access Technology, Int",
+                description:"2014 - Current"
+            }
+        ]
+    },
+    {
+        title: "Education & Certification",
+        items: [
+            {
+                header:"Certificate in full-stack development",
+                sub:"University of Minnesota Bootcamp",
+                description:"2021"
+            },
+            {
+                header:"BA in Computer Science",
+                sub:"University of Minnesota Morris",
+                description:"2014"
+            }
+        ]
+    }
+]
+
+export {AboutMe, Skills}
