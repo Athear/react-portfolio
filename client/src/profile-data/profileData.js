31efb7fb19a2aafa9c04b63d66b4815a65e729e8
Todd Malone <toddomalone@gmail.com>
2021-05-23 14:50:17 -0500

    added projects to profile data

--- client/src/profile-data/profileData.js
+++ client/src/profile-data/profileData.js
@@ -1,36 +1,45 @@
 export default [
     {
-        image: 'image-1',
+        image: 'imgae-1',
+        title: 'kARaoke',
+        tagline: 'An AR Karaoke experience',
+        deployed:"https://coders-delight-karaoke.herokuapp.com/",
+        repo:"https://github.com/Athear/kARaoke"
+    },
+    {
+        image: 'noun_Dog_1539664.png',
         title: 'Park Pack',
-        deployed:"",
-        repo:""
+        tagline: 'A social site for making doggy play dates!',
+        deployed:"https://park-pack.herokuapp.com/",
+        repo:"https://github.com/Athear/park-pack"
+    },
+    {
+        image: 'cornucopedia-thumb.jpg',
+        title: 'Cornucopedia',
+        tagline: 'A stripped-down recipe search application.',
+        deployed:"https://athear.github.io/cornucopedia/",
+        repo:"https://github.com/Athear/cornucopedia"
+    },
+    {
+        image: 'weather-icon.jpg',
+        title: 'Weather Dashboard',
+        tagline:'Get a 5-day forecast for a city of your choice.',
+        deployed:"https://athear.github.io/Weather-Dashboard/",
+        repo:"https://github.com/Athear/Weather-Dashboard"
+    },
+    {
+        image: 'question_mark_PNG129.png',
+        title: 'Quiz Framework',
+        tagline: 'Quiz yourself, quiz your friends! Designed to be configurable',
+        deployed:"https://athear.github.io/Quiz-Framework",
+        repo:"https://github.com/Athear/Quiz-Framework"
     },
     {
         image: 'image-1',
-        title: 'Getting tickets to the big show',
-        deployed:"",
-        repo:""
+        title: 'budgeteer-offline',
+        tagline: 'A PWA budgeting app. Track finances on- and off-line',
+        deployed:"https://budgeteer-offline.herokuapp.com/",
+        repo:"https://github.com/Athear/budgeteer-offline"
     },
-    {
-        image: 'image-1',
-        title: 'Getting tickets to the big show',
-        deployed:"",
-        repo:""
-    },
-    {
-        image: 'image-1',
-        title: 'Getting tickets to the big show',
-        deployed:"",
-        repo:""
-    },
-    {
-        image: 'image-3',
-        category: 'Development',
-        title: 'Getting tickets to the big show'
-    },
-    {
-        image: 'image-4',
-        category: 'Development',
-        title: 'Getting tickets to the big show'
-    }
+
 ]
