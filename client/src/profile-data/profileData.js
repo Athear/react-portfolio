0aaf56ce997e115bc0f11d3b4f9c011bf9ceced7
Todd Malone <toddomalone@gmail.com>
2021-05-24 23:36:54 -0500

    linked personalized images

--- client/src/profile-data/profileData.js
+++ client/src/profile-data/profileData.js
@@ -1,41 +1,41 @@
 export default [
     {
-        image: 'imgae-1',
+        image: 'image-1',
         title: 'kARaoke',
         tagline: 'An AR Karaoke experience',
         deployed:"https://coders-delight-karaoke.herokuapp.com/",
         repo:"https://github.com/Athear/kARaoke"
     },
     {
-        image: 'noun_Dog_1539664.png',
+        image: 'image-2',
         title: 'Park Pack',
         tagline: 'A social site for making doggy play dates!',
         deployed:"https://park-pack.herokuapp.com/",
         repo:"https://github.com/Athear/park-pack"
     },
     {
-        image: 'cornucopedia-thumb.jpg',
+        image: 'image-3',
         title: 'Cornucopedia',
         tagline: 'A stripped-down recipe search application.',
         deployed:"https://athear.github.io/cornucopedia/",
         repo:"https://github.com/Athear/cornucopedia"
     },
     {
-        image: 'weather-icon.jpg',
+        image: 'image-4',
         title: 'Weather Dashboard',
         tagline:'Get a 5-day forecast for a city of your choice.',
         deployed:"https://athear.github.io/Weather-Dashboard/",
         repo:"https://github.com/Athear/Weather-Dashboard"
     },
     {
-        image: 'question_mark_PNG129.png',
+        image: 'image-5',
         title: 'Quiz Framework',
         tagline: 'Quiz yourself, quiz your friends! Designed to be configurable',
         deployed:"https://athear.github.io/Quiz-Framework",
         repo:"https://github.com/Athear/Quiz-Framework"
     },
     {
-        image: 'image-1',
+        image: 'image-6',
         title: 'budgeteer-offline',
         tagline: 'A PWA budgeting app. Track finances on- and off-line',
         deployed:"https://budgeteer-offline.herokuapp.com/",
