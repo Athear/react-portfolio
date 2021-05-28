83d68b8588324081bda0ef59eb0bbf02c5c28144
Todd Malone <toddomalone@gmail.com>
2021-05-27 22:53:20 -0500

    Updates

---
+++ precache-manifest.0e80785043e2709db9877f56a277804f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f3b088c6589c7994d27d8b2d457002",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e1f51f228aa9b37f30a1",
+    "url": "/react-portfolio/static/css/2.c300a1fb.chunk.css"
+  },
+  {
+    "revision": "10ff950ad59f2754955b",
+    "url": "/react-portfolio/static/css/main.5d4b4341.chunk.css"
+  },
+  {
+    "revision": "e1f51f228aa9b37f30a1",
+    "url": "/react-portfolio/static/js/2.4e90f181.chunk.js"
+  },
+  {
+    "revision": "10ff950ad59f2754955b",
+    "url": "/react-portfolio/static/js/main.1088ca9b.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
