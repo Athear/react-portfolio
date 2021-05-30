4b55f54ea3b3790045506d518e6198a7d6092151
Todd Malone <toddomalone@gmail.com>
2021-05-29 20:02:22 -0500

    Updates

---
+++ precache-manifest.11752b9ce59bc3501062e9e8afef340e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278c0f431f4273a84238037792658048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b965be4c60d3ea581290",
+    "url": "/static/css/2.49c3c7a2.chunk.css"
+  },
+  {
+    "revision": "285680040b9d095af58a",
+    "url": "/static/css/main.c50b8243.chunk.css"
+  },
+  {
+    "revision": "b965be4c60d3ea581290",
+    "url": "/static/js/2.4e90f181.chunk.js"
+  },
+  {
+    "revision": "285680040b9d095af58a",
+    "url": "/static/js/main.132ecbf4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
